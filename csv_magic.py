cd482cbd7d04509f6cb1b2d9026f88db4b1af57b
django <dieter.skroblin@gmx.de>
2022-06-08 23:34:10 +0200

    implement double_spin_box +-

--- csv_magic.py
+++ csv_magic.py
@@ -1,28 +1,45 @@
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# Press Strg+F8 to toggle the breakpoint.
-
 import sys
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QApplication, QMainWindow, QToolBar, QFileDialog
+from PySide6.QtWidgets import (QApplication, QMainWindow, QToolBar, QFileDialog, QGridLayout, QLabel, QDoubleSpinBox,
+                               QWidget)
+
+SPINBOX_DEFAULT = 1.5
+WIDTH = 600
+HEIGHT = 400
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(300, 300, 600, 400)
-        self.setWindowTitle("CSV Magic")
-
+        window = QWidget()
         toolbar = QToolBar()
-        self.addToolBar(toolbar)
-        file_menu = self.menuBar().addMenu("&File")
+        grid = QGridLayout()
+
+        double_spin_box_plus = QDoubleSpinBox()
+        double_spin_box_plus.setPrefix("+ ")
+        double_spin_box_plus.setValue(SPINBOX_DEFAULT)
+        double_spin_box_minus = QDoubleSpinBox()
+        double_spin_box_minus.setPrefix("- ")
+        double_spin_box_minus.setValue(SPINBOX_DEFAULT)
+
         load_action = QAction("Load...", self, shortcut="Ctrl+L", triggered=self.load)
-        file_menu.addAction(load_action)
-        toolbar.addAction(load_action)
         exit_action = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+
+        file_menu = self.menuBar().addMenu("&File")
+        file_menu.addAction(load_action)
         file_menu.addAction(exit_action)
 
+        toolbar.addAction(load_action)
+
+        grid.addWidget(double_spin_box_minus, 0, 0)
+        grid.addWidget(double_spin_box_plus, 0, 1)
+
+        self.addToolBar(toolbar)
+        window.setLayout(grid)
+        self.setCentralWidget(window)
+        self.setGeometry(300, 300, WIDTH, HEIGHT)
+        self.setWindowTitle("CSV Magic")
         self.show()
 
     def load(self):
@@ -32,11 +49,8 @@ class Window(QMainWindow):
             print(file_name[0])
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     window = Window()
     sys.exit(app.exec())
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
