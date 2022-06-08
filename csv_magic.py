a54098767e8327c675d4dbdb99a70247dc0a7244
django <dieter.skroblin@gmx.de>
2022-06-08 22:17:08 +0200

    implement toolbar and file_menu

--- csv_magic.py
+++ csv_magic.py
@@ -2,16 +2,32 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # Press Strg+F8 to toggle the breakpoint.
 
-from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QToolBar
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setGeometry(300, 300, 600, 400)
-        self.setWindowTitle("Window Title")
+        self.setWindowTitle("CSV Magic")
+
+        toolbar = QToolBar()
+        self.addToolBar(toolbar)
+        file_menu = self.menuBar().addMenu("&File")
+        load_action = QAction("Load...", self, shortcut="Ctrl+L", triggered=self.load)
+        file_menu.addAction(load_action)
+        toolbar.addAction(load_action)
+        exit_action = QAction("E&xit", self, shortcut="Ctrl+Q", triggered=self.close)
+        file_menu.addAction(exit_action)
+
         self.show()
+
+    def load(self):
+        pass
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
